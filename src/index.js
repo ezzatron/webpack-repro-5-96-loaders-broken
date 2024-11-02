d4fd22d5cf267562bb001f717fe2be20fb645e11
Erin <contact@erin.id.au>
2024-11-02 22:53:27 +1000

    Polish reproduction

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ if (
   console.log("✅ Loader is working");
   console.log();
   console.log("Loaded asset should be", { asset: "/asset.public.txt" });
-  console.log("Loaded is actually", asset);
+  console.log("Loaded asset is actually", asset);
 } else {
   console.error("❌ Loader is NOT working");
   console.error();
   console.error("Loaded asset should be", { asset: "/asset.public.txt" });
-  console.error("Loaded is actually", asset);
+  console.error("Loaded asset is actually", asset);
   process.exit(1);
 }
