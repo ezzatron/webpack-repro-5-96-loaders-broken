2e11a92ae4972620f8f40d1d9749cf9a5ae7ebe3
Erin <contact@erin.id.au>
2024-11-02 22:55:30 +1000

    Polish reproduction

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ if (
 ) {
   console.log("✅ Loader is working");
   console.log();
-  console.log("Loaded asset should be", { asset: "/asset.public.txt" });
-  console.log("Loaded asset is actually", asset);
+  console.log("Loaded asset should be:  ", { asset: "/asset.public.txt" });
+  console.log("Loaded asset is actually:", asset);
 } else {
   console.error("❌ Loader is NOT working");
   console.error();
-  console.error("Loaded asset should be", { asset: "/asset.public.txt" });
-  console.error("Loaded asset is actually", asset);
+  console.error("Loaded asset should be:  ", { asset: "/asset.public.txt" });
+  console.error("Loaded asset is actually:", asset);
   process.exit(1);
 }
